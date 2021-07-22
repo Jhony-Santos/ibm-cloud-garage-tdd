89c1a93bf4304cee621d5168f27064698f26c6ce
jntd <jntd@gft.com>
2021-07-22 16:33:25 -0300

    Pass the second test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.js
@@ -1,4 +1,4 @@
-const fahrenheitToCelsius = () => { return 0};
+const fahrenheitToCelsius = (fahrenheit) => (fahrenheit-32) * 5/9 ;
 
 module.exports = { fahrenheitToCelsius };
 
--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -16,6 +16,9 @@ describe('a fahrenheitToCelsius function should', () => {
   it('return 0 for 32',() => {
     expect(fahrenheitToCelsius(32)).toBe(0);
   });
+  it('return 10 for 50',()=>{
+    expect(fahrenheitToCelsius(50)).toBe(10);
+  });
   it.todo('return 0 for 32');
   it.todo('return 10 for 50');
   it.todo('return 100 for 212');
