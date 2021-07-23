95236c548a027d80a74ffa78db92702080925621
jntd <jntd@gft.com>
2021-07-23 12:06:38 -0300

    third, four e five tests

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -19,6 +19,15 @@ describe('a fahrenheitToCelsius function should', () => {
   it('return 10 for 50',()=>{
     expect(fahrenheitToCelsius(50)).toBe(10);
   });
+  it('return 100 for 212',()=>{
+    expect(fahrenheitToCelsius(212)).toBe(100);
+  });
+  it('return -40 for -40',()=>{
+    expect(fahrenheitToCelsius(-40)).toBe(-40);
+  });
+  it('return -273.15 for -459.67',()=>{
+    expect(fahrenheitToCelsius(-459.67)).toBe(-273.15);
+  });
   it.todo('return 0 for 32');
   it.todo('return 10 for 50');
   it.todo('return 100 for 212');
